77e90bb9519e47b0708e0175b0df654d7a2fffca
capelliludivine <ludivine@kbdev.io>
2020-02-06 15:21:10 +0100

    Link beetween view

---
+++ Android/ProjetMSPR/app/src/main/java/com/example/projetmspr/Connexion.java
@@ -0,0 +1,33 @@
+package com.example.projetmspr;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Connexion extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.connexion);  //Définition de la vue principale
+        Button button = (Button) findViewById(R.id.connexionButton);   //Appel de buttonConnexion"
+        button.setOnClickListener(new View.OnClickListener()      //Creation du listener sur ce bouton
+        {
+            public void onClick(View actuelView)    //au clic sur le bouton
+            {
+                Intent intent = new Intent(Connexion.this, Accueil.class);  //Lancer l'activité Coupon
+                startActivity(intent);    //Afficher la vue-------------------------------------------------------------------------------------------------------------------------------------
+            }
+        });
+    }
+}
+
+
+
+
---
+++ Android/ProjetMSPR/app/src/main/java/com/example/projetmspr/Coupon.java
@@ -0,0 +1,16 @@
+package com.example.projetmspr;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Coupon extends AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) //A la   creation de la vue
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.coupon); //Afficher la vue portant le nom "coupon"
+    }
+
+}
