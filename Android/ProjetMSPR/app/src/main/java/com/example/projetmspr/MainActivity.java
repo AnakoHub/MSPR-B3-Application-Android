d6cada0b8ebc908fae906b168f8c188806f6e23e
capelliludivine <ludivine@kbdev.io>
2020-02-06 15:21:10 +0100

    Link beetween view

--- Android/ProjetMSPR/app/src/main/java/com/example/projetmspr/MainActivity.java
+++ Android/ProjetMSPR/app/src/main/java/com/example/projetmspr/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.projetmspr;
 
-import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);  //Définition de la vue principale
+       /* ImageView imageView = (ImageView) findViewById(R.id.coupon1);   //Appel de l'imageView2"
+        imageView.setOnClickListener(new View.OnClickListener()      //Creation du listener sur ce bouton
+        {
+            public void onClick(View actuelView)    //au clic sur le bouton
+            {
+                Intent intent = new Intent(MainActivity.this, Coupon.class);  //Lancer l'activité Coupon
+                startActivity(intent);    //Afficher la vue
+            }
+        });
+    }*/
     }
 }
