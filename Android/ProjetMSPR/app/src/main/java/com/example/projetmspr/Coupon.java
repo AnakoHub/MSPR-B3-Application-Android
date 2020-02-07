1602c98618079674c8cb7e48909be0bf2d596d90
capelliludivine <ludivine@kbdev.io>
2020-02-07 10:26:21 +0100

    Update "coupon.xml"

--- Android/ProjetMSPR/app/src/main/java/com/example/projetmspr/Coupon.java
+++ Android/ProjetMSPR/app/src/main/java/com/example/projetmspr/Coupon.java
@@ -3,14 +3,40 @@ package com.example.projetmspr;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Coupon extends AppCompatActivity {
-    public void onCreate(Bundle savedInstanceState) //A la création de la vue
+    public void onCreate(Bundle savedInstanceState) //A la   creation de la vue
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.coupon); //Afficher la vue portant le nom "coupon"
+        Button button = (Button) findViewById(R.id.button);   //Appel du bouton pour afficher la liste de coupon
+        button.setOnClickListener(new View.OnClickListener()      //Creation du listener sur ce bouton
+        {
+            public void onClick(View actuelView)    //au clic sur le bouton
+            {
+                showBackBtn();
+            }
+        });
+    }
+
+    protected void showBackBtn(){
+        TextView textView=findViewById(R.id.textView3);
+        if(textView!=null){
+            textView.setVisibility(View.VISIBLE);
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+
+        }
     }
 
 }
