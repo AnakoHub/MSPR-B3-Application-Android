279ba35c5784271178c6d2ec1d07b323e8e3ef43
capelliludivine <ludivine@kbdev.io>
2020-02-11 17:26:19 +0100

    Add arrow for return

--- Android/ProjetMSPR/app/src/main/java/com/example/projetmspr/MainActivity.java
+++ Android/ProjetMSPR/app/src/main/java/com/example/projetmspr/MainActivity.java
@@ -7,15 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);  //Définition de la vue principale
+        showBackBtn();
        /* ImageView imageView = (ImageView) findViewById(R.id.coupon1);   //Appel de l'imageView2"
         imageView.setOnClickListener(new View.OnClickListener()      //Creation du listener sur ce bouton
         {
@@ -26,5 +28,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }*/
+    }
+
+    protected void showBackBtn(){
+        ImageView imageView=findViewById(R.id.imageViewClose);
+        if(imageView!=null) {
+            imageView.setVisibility(View.VISIBLE);
+            imageView.setOnClickListener(this);
+        }
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imageViewClose:
+                finish();
+                break;
+        }
     }
 }
