30f196c470d705fe56a1cfe7357ed1abd0c4cc9f
Anael <anaelakue60@gmail.com>
2020-02-12 15:08:25 +0100

    Creation of MsprActivity for link between android app and webservice

---
+++ Android/ProjetMSPR/app/src/main/java/com/example/webservice/MsprActivity.java
@@ -0,0 +1,13 @@
+package com.example.webservice;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MsprActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
