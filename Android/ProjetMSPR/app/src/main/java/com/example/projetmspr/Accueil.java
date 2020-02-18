9786ad62b9dcc7cbd57e7872bf8868edb97c1f73
capelliludivine <ludivine@kbdev.io>
2020-02-18 14:26:19 +0100

    Javadoc on Accueil class

--- Android/ProjetMSPR/app/src/main/java/com/example/projetmspr/Accueil.java
+++ Android/ProjetMSPR/app/src/main/java/com/example/projetmspr/Accueil.java
@@ -8,22 +8,58 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * <b>Accueil is the classe will execute the functionnality of Accueil view  </b>
+ * 
+ * @author Ludivine CAPELLI
+ */
 public class Accueil extends AppCompatActivity implements View.OnClickListener {
+    /**
+     *
+     * @param savedInstanceState
+     */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.accueil);  //Définition de la vue principale
+        /**
+         *
+         * Method to define the main view
+         *
+         */
+        setContentView(R.layout.accueil);
+        /**
+         * method showBackButton is called
+         */
         showBackBtn();
-        Button button = (Button) findViewById(R.id.listeDeCouponButton);   //Appel du bouton qui permet d'acceder à la liste de coupon de réduction
-        button.setOnClickListener(new View.OnClickListener()      //Creation du listener sur ce bouton
+        /**
+         *
+         * button is called for give access to the coupon list
+         *
+         */
+        Button button = (Button) findViewById(R.id.listeDeCouponButton);
+        /**
+         *
+         * method to create listener on the button
+         *
+         */
+        button.setOnClickListener(new View.OnClickListener()
         {
-            public void onClick(View actuelView)    //au clic sur le bouton
+            /**
+             *
+             * Method to display the view when clicking on the button
+             *
+             * @param actuelView
+             */
+            public void onClick(View actuelView)
             {
-                Intent intent = new Intent(Accueil.this, MainActivity.class);  //Lancer l'activité Coupon
-                startActivity(intent);    //Afficher la vue
+                Intent intent = new Intent(Accueil.this, MainActivity.class);
+                startActivity(intent);
             }
         });
     }
 
+    /**
+     * Method to show the image view when the method is call
+     */
     protected void showBackBtn(){
         ImageView imageView=findViewById(R.id.imageViewClose);
         if(imageView!=null) {
@@ -32,7 +68,11 @@ public class Accueil extends AppCompatActivity implements View.OnClickListener {
         }
     }
 
-
+    /**
+     * Method to display the view when clicking on the image view
+     *
+     * @param v
+     */
     @Override
     public void onClick(View v) {
         switch (v.getId()){
