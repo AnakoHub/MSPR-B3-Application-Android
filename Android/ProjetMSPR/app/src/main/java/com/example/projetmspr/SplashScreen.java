654c81c8791da30204e1fdaeeed955f8d6929aa9
capelliludivine <ludivine@kbdev.io>
2020-02-25 11:46:38 +0100

    Add SplashScreen on app

---
+++ Android/ProjetMSPR/app/src/main/java/com/example/projetmspr/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.example.projetmspr;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+    private static int SPLASH_TIME_OUT = 1500;
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, Connexion.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
